30831c12da87a6c60e5b63fd4cd992c74431c406
protodrama <juanfranrodri@gmail.com>
2016-03-12 21:02:40 +0100

    Interfaz de conexión
    Creación de la interfaz de conexión (Abierta a cambios)

--- Farmacop/Form1.Designer.cs
+++ Farmacop/Form1.Designer.cs
@@ -28,23 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Formulario));
+            this.pctbxFarmacop = new System.Windows.Forms.PictureBox();
+            this.lblIniciarSesion = new System.Windows.Forms.Label();
+            this.tbxCorreo = new System.Windows.Forms.TextBox();
+            this.tbxPass = new System.Windows.Forms.TextBox();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pctbxFarmacop)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pctbxFarmacop
+            // 
+            this.pctbxFarmacop.Image = ((System.Drawing.Image)(resources.GetObject("pctbxFarmacop.Image")));
+            this.pctbxFarmacop.Location = new System.Drawing.Point(489, 73);
+            this.pctbxFarmacop.Name = "pctbxFarmacop";
+            this.pctbxFarmacop.Size = new System.Drawing.Size(418, 62);
+            this.pctbxFarmacop.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctbxFarmacop.TabIndex = 0;
+            this.pctbxFarmacop.TabStop = false;
+            // 
+            // lblIniciarSesion
+            // 
+            this.lblIniciarSesion.AutoSize = true;
+            this.lblIniciarSesion.Font = new System.Drawing.Font("Arial Black", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIniciarSesion.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.lblIniciarSesion.Location = new System.Drawing.Point(553, 152);
+            this.lblIniciarSesion.Name = "lblIniciarSesion";
+            this.lblIniciarSesion.Size = new System.Drawing.Size(280, 50);
+            this.lblIniciarSesion.TabIndex = 1;
+            this.lblIniciarSesion.Text = "Iniciar sesión";
+            // 
+            // tbxCorreo
+            // 
+            this.tbxCorreo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxCorreo.Location = new System.Drawing.Point(579, 229);
+            this.tbxCorreo.Name = "tbxCorreo";
+            this.tbxCorreo.Size = new System.Drawing.Size(220, 26);
+            this.tbxCorreo.TabIndex = 1;
+            // 
+            // tbxPass
+            // 
+            this.tbxPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxPass.Location = new System.Drawing.Point(579, 290);
+            this.tbxPass.Name = "tbxPass";
+            this.tbxPass.PasswordChar = '*';
+            this.tbxPass.Size = new System.Drawing.Size(220, 26);
+            this.tbxPass.TabIndex = 2;
+            this.tbxPass.UseSystemPasswordChar = true;
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrar.Location = new System.Drawing.Point(640, 336);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(94, 32);
+            this.btnEntrar.TabIndex = 3;
+            this.btnEntrar.Text = "Entrar";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(579, 210);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Correo electrónico:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(579, 274);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Contraseña:";
             // 
             // Formulario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1419, 821);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEntrar);
+            this.Controls.Add(this.tbxPass);
+            this.Controls.Add(this.tbxCorreo);
+            this.Controls.Add(this.lblIniciarSesion);
+            this.Controls.Add(this.pctbxFarmacop);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Formulario";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Farmacop";
+            ((System.ComponentModel.ISupportInitialize)(this.pctbxFarmacop)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pctbxFarmacop;
+        private System.Windows.Forms.Label lblIniciarSesion;
+        private System.Windows.Forms.TextBox tbxCorreo;
+        private System.Windows.Forms.TextBox tbxPass;
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
